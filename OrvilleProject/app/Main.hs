ccbc0020c96d400e22d1671ec2bed7a7555f63ad
Ruchita Ganurkar <63845947+RuchitaGanurkar@users.noreply.github.com>
2024-08-07 10:38:56 +0530

    Add files via upload

--- OrvilleProject/app/Main.hs
+++ OrvilleProject/app/Main.hs
@@ -1,20 +1,21 @@
+module Main where
 
-module main where
 
-
-  
+import qualified Orville.PostgreSQL.AutoMigration as A
+import qualified Orville.PostgreSQL as O
 import Network.Wai.Handler.Warp (run)
-import Servant
-import Implementation.Config (AppConfig(..), initialConfig)
-import Implementation.Core (AppMonad, runApp)
-import Implementation.Routes (api, appServer)
-import Implementation.DB (migrateDB)
+import Implementation
 
-app :: AppConfig -> Application
-app cfg = logStdoutDev $ serve api (hoistServer api (runApp cfg) appServer)
 
 main :: IO ()
 main = do
-  appConfig <- initialConfig
-  runDBMigrate appConfig
-  run 3000 (app appConfig)
+  putStrLn "Application Running On Port 3000"
+  maybeConfig <- getPostgresConfig
+  case maybeConfig of
+    Just dbConfig -> do
+      let connOpts = connectionOptions (pgConfigToString dbConfig)
+      pool <- O.createConnectionPool connOpts
+      O.runOrville pool $ A.autoMigrateSchema A.defaultOptions [A.SchemaTable studentTable]
+      let appConfig = AppConfig { appDbPool = pool }
+      run 3000 (app appConfig)
+    Nothing -> error "Database Configuration Not Found"
