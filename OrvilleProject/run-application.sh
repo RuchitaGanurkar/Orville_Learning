9e0a2dacb3ac6f2b8ad581c20a22d65694b73641
Karthic kumar <vn08264@heb.com>
2024-08-21 17:06:42 +0530

    added gitattributes

--- OrvilleProject/run-application.sh
+++ OrvilleProject/run-application.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Set environment variables
-export POSTGRES_DB="orville_project"
-export POSTGRES_USER="ruchita"
-export POSTGRES_PASSWORD="qwerty"
-export DB_CONN_STRING="postgresql://ruchita:qwerty@database:5432/orville_project"
+set -o allexport
+source .env
+set +o allexport
 
 # Run Docker Compose
 docker-compose up
